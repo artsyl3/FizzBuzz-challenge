dc93b096bee8d2f500c7f1749decd19ff68ccc24
Hossein Ghanbari <118113234+artsyl3@users.noreply.github.com>
2025-12-26 13:22:07 +0100

    Update Program.cs

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -2,7 +2,7 @@
     if (i % 3 == 0 && i % 5 == 0) {
         Console.WriteLine(i + " - FizzBuzz ");
     }else if (i % 5 == 0) {
-        Console.WriteLine(i + " - Buzz ");
+        Console.WriteLine(i + " - Buzzzzz ");
     }else if (i % 3 == 0) {
         Console.WriteLine(i + " - Fizz ");
     } else { Console.WriteLine(i);}
